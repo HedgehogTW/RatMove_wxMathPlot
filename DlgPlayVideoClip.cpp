826326ac185ddf9ce29df21b951da81ac1d9af98
HedgehogTW <pragueczech@gmail.com>
2016-11-21 17:34:19 +0800

    * create another dialog, unfinished

---
+++ DlgPlayVideoClip.cpp
@@ -0,0 +1,11 @@
+#include "DlgPlayVideoClip.h"
+
+DlgPlayVideoClip::DlgPlayVideoClip(wxWindow* parent)
+    : DlgPlayVideoClipBase(parent)
+{
+}
+
+DlgPlayVideoClip::~DlgPlayVideoClip()
+{
+}
+
---
+++ DlgPlayVideoClip.h
@@ -0,0 +1,11 @@
+#ifndef DLGPLAYVIDEOCLIP_H
+#define DLGPLAYVIDEOCLIP_H
+#include "play_video_clip.h"
+
+class DlgPlayVideoClip : public DlgPlayVideoClipBase
+{
+public:
+    DlgPlayVideoClip(wxWindow* parent);
+    virtual ~DlgPlayVideoClip();
+};
+#endif // DLGPLAYVIDEOCLIP_H
